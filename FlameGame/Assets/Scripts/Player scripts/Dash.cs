ecc0ec578159169fcad0c8d3cd6adccc156ee5a9
Akkarin604 <kacper@uminski.co>
2025-03-05 09:52:48 +0100

    movement i dash

---
+++ FlameGame/Assets/Scripts/Player scripts/Dash.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Dash : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float strength = 40f;
+    [SerializeField] private float delay = 0.1f;
+
+    public bool canDash = true;
+    public float cooldownValue = 1.5f;
+
+    public UnityEvent OnBegin;
+    public UnityEvent OnDone;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void PlayDash()
+    {
+        if(canDash)
+        {
+            canDash = false;
+            OnBegin?.Invoke();
+            rb.AddForce(rb.linearVelocity.normalized * strength, ForceMode2D.Impulse);
+            StartCoroutine(Reset());
+        }
+        
+    }
+    
+    private IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(delay);
+        OnDone?.Invoke();
+        StartCoroutine(Cooldown());
+    }
+
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldownValue);
+        canDash = true;
+    }
+    void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            PlayDash();
+        }
+    }
+}
--- FlameGame/Assets/Scripts/Player scripts/PlayerMovement.cs
+++ FlameGame/Assets/Scripts/Player scripts/PlayerMovement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float maxSpeed = 20f;
-    [SerializeField] private float acceleration = 5f;
-    [SerializeField] private float decelaratiom = 5f;
-    [SerializeField] private bool dashActive = false;
+    public float maxSpeed = 10f;
+    [SerializeField] private float acceleration = 32f;
+    [SerializeField] private float decelaration = 4f;
+
+    private bool tooHighSpeed;
 
     private Rigidbody2D rb;
 
@@ -15,20 +16,33 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+    private void Update()
+    {
+        if(rb.linearVelocity.magnitude > maxSpeed)
+        {
+            tooHighSpeed = true;
+        }
+        else
+        {
+            tooHighSpeed = false;
+        }
+    }
     private void FixedUpdate()
     {
-        if (InputManager.Movement != Vector2.zero)
+        if(InputManager.Movement != Vector2.zero)
         {
-            rb.AddForce(InputManager.Movement * acceleration, ForceMode2D.Force);
-
-            if (rb.linearVelocity.magnitude > maxSpeed)
+            if (tooHighSpeed)
             {
-                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+                rb.AddForce(rb.linearVelocity * -decelaration, ForceMode2D.Force);
+            } 
+            else
+            {
+                rb.AddForce(InputManager.Movement * acceleration, ForceMode2D.Force);
             }
-        } else
+        } 
+        else
         {
-            rb.AddForce(rb.linearVelocity * -decelaratiom, ForceMode2D.Force);
+            rb.AddForce(rb.linearVelocity * -decelaration * 4, ForceMode2D.Force);
         }
     }
 }
