6df2dbd9c9afd2b7a3eb1ea3423ccdcef942538e
Akkarin604 <kacper@uminski.co>
2025-03-08 18:33:47 +0100

    player attack

--- FlameGame/.github/copilot-instructions.md
+++ FlameGame/.github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: FlameGame
 - Unity version: Unity 6000.0.40f1
 - Active game object:
-  - Name: ENEMY (2)
-  - Tag: Enemy
-  - Layer: Enemy
+  - Name: PLAYER
+  - Tag: Player
+  - Layer: Player
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- FlameGame/Assets/Scripts/Player scripts/PlayerAttack.cs
+++ FlameGame/Assets/Scripts/Player scripts/PlayerAttack.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private GameObject crosshair;
-
+    [SerializeField] private GameObject cursor;
     private Rigidbody2D rb;
-
+    private Rigidbody2D cloneRb;
+    public GameObject projectile;
+    private Vector3 mouseScreenPosition;
+    private Vector3 mouseWorldPosition;
+    private Vector3 mouseGamePos;
 
     private void Awake()
     {
@@ -15,5 +20,30 @@ public class PlayerAttack : MonoBehaviour
     {
         Vector2 direction = (rb.linearVelocity.normalized * 3);
         crosshair.transform.localPosition = direction;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            PlayerBasicShoot();
+        }
+
+        mouseScreenPosition = Mouse.current.position.ReadValue();
+        mouseScreenPosition.z = 0f;
+        mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+        mouseGamePos = new Vector3(mouseWorldPosition.x, mouseWorldPosition.y, transform.position.z);
+        cursor.transform.position = mouseGamePos;
+    }
+
+    private void PlayerBasicShoot()
+    {
+        
+        GameObject clone;
+        clone = Instantiate(projectile, transform.position, Quaternion.identity);
+        clone.SetActive(true);
+        cloneRb = clone.GetComponent<Rigidbody2D>();
+        Vector3 direction = (mouseGamePos - clone.transform.position);
+        cloneRb.linearVelocity = direction.normalized * 10f;
+
     }
 }
