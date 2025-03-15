0a5b10d394d4c6d80735b23af089f8279e20bc1b
Akkarin604 <kacper@uminski.co>
2025-03-15 09:36:50 +0100

    player health

--- FlameGame/Assets/Scripts/Player scripts/PlayerDeath.cs
+++ FlameGame/Assets/Scripts/Player scripts/PlayerDeath.cs
@@ -3,24 +3,40 @@ using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float playerHealth;
+    [SerializeField] private float basicEnemyDamage = 20f;
+    [SerializeField] private float passiveDegeneration = 1f;
+    [SerializeField] private float playerStartingHealth = 100f;
+
+
+
+
     void Start()
     {
-        
+        playerHealth = playerStartingHealth;
+        InvokeRepeating(OncePerSecound, 0, 1.0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(playerHealth <= 0)
+        {
+            Debug.Log("player died");
+            transform.position = Vector3.zero;
+        }
+    }
+
+    private void OncePerSecound()
+    {
+        playerHealth = playerHealth - passiveDegeneration; //candle mechanic
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("EnemyHitbox"))
         {
-            Debug.Log("player Died");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Debug.Log("player got hit");
+            playerHealth = playerHealth - basicEnemyDamage;
         }
     }
 }
