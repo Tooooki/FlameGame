8c0142de0a281a4f6249a4fa404b4446615bb8cc
Akkarin604 <kacper@uminski.co>
2025-03-25 21:38:40 +0100

    movement (sharp)

--- FlameGame/Assets/Scripts/Player scripts/PlayerMovement.cs
+++ FlameGame/Assets/Scripts/Player scripts/PlayerMovement.cs
@@ -5,44 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float maxSpeed = 10f;
-    [SerializeField] private float acceleration = 32f;
-    [SerializeField] private float decelaration = 4f;
-
-    private bool tooHighSpeed;
 
     private Rigidbody2D rb;
+
+    public bool movementActive = true;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    private void Update()
-    {
-        if(rb.linearVelocity.magnitude > maxSpeed)
-        {
-            tooHighSpeed = true;
-        }
-        else
-        {
-            tooHighSpeed = false;
-        }
-    }
+
     private void FixedUpdate()
     {
-        if(InputManager.Movement != Vector2.zero)
+        if(movementActive)
         {
-            if (tooHighSpeed)
-            {
-                rb.AddForce(rb.linearVelocity * -decelaration, ForceMode2D.Force);
-            } 
-            else
-            {
-                rb.AddForce(InputManager.Movement * acceleration, ForceMode2D.Force);
-            }
-        } 
-        else
-        {
-            rb.AddForce(rb.linearVelocity * -decelaration * 4, ForceMode2D.Force);
+            rb.linearVelocity = InputManager.Movement * maxSpeed;
         }
     }
 }
