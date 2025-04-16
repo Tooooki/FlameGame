be66ed002ad729181fc52a784b94de923f7c87da
R52W <stachdee@gmail.com>
2025-04-16 18:34:26 +0200

    działa przy level up karty

--- FlameGame/Assets/Scripts/Experience.cs
+++ FlameGame/Assets/Scripts/Experience.cs
@@ -35,6 +35,9 @@ public class Experience : MonoBehaviour
             expNeeded = 100 + (Level * 50);
             healthScript.playerMaxHealth = healthScript.playerMaxHealth + 20;
             healthScript.playerHealth = healthScript.playerMaxHealth;
+
+            GameManager.Instance.ChangeState(GameManager.GameState.CardSelection); // 👈 show cards!
+
         }
         else
         {
